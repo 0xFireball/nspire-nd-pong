53cdaf979f71f5a846b99984230f85bb3d746fb3
0xFireball <0xFireball@outlook.com>
2017-08-17 11:54:29 -0700

    update ball motion

--- sprites/ball.cpp
+++ sprites/ball.cpp
@@ -10,16 +10,24 @@ void Ball::update(float dt) {
         // TODO: give opponent a point
         // respawn the ball, normally
         // but for now, bounce
+        this->x = 0;
         this->_vel.setX(-this->_vel.getX());
     }
     if (pos.getX() + this->width > Reg::game->width) {
         // TODO: give opponent a point
         // respawn the ball, normally
         // but for now, bounce
+        this->x = Reg::game->width - this->width;
         this->_vel.setX(-this->_vel.getX());
     }
-    if (pos.getY() < 0 || pos.getY() - this->width > Reg::game->height) {
+    if (pos.getY() < 0) {
         // reflect y-vel
+        this->y = 0;
+        this->_vel.setY(-this->_vel.getY());
+    }
+    if (pos.getY() + this->width > Reg::game->height) {
+        // reflect y-vel
+        this->y = Reg::game->height - this->width;
         this->_vel.setY(-this->_vel.getY());
     }
 
